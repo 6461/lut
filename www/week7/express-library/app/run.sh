141fb2a72df4fc6df5bf6c47d4793db55221d97f
Miikka <miikka.mattola@gmail.com>
2018-11-22 19:48:40 +0000

    Create a wait script for database

--- www/week7/express-library/app/run.sh
+++ www/week7/express-library/app/run.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
+DB_HOST=mongo
+DB_PORT=27017
+
+# Wait for Mongo database to start
+until timeout 1 bash -c "</dev/tcp/${DB_HOST}/${DB_PORT}"
+do
+	echo "Waiting for Mongo to start..."
+	sleep 1
+done
 
 # Populate database with test data
-node populatedb mongodb://mongo:27017
+node populatedb mongodb://$DB_HOST:$DB_PORT
 # Debug mode
 #DEBUG=express-library:* npm run devstart
 # normal start
